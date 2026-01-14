ffcc714d8d239ec3772501ceff63e7fc98ff3284
ssyang <ssyang@elpusk.co.kr>
2026-01-14 11:57:29 +0900

    add tailwindcss

--- packages/app/webmapper/vite.config.ts
+++ packages/app/webmapper/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -8,7 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   // Configured for deployment at https://elpusk.github.io/library.js.coffee.2nd/app/webmapper/
   base: process.env.VITE_BASE_PATH || '/library.js.coffee.2nd/app/webmapper/',
   resolve: {
